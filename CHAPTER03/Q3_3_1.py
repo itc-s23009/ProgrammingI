d255c8cb3ed85b85925c1d72b8f5c5968a4972af
Yuto Kohama <s23009@std.it-college.ac.jp>
2023-07-12 06:18:00 +0000

    Q3_2, Q3_3

---
+++ CHAPTER03/Q3_3_1.py
@@ -0,0 +1,10 @@
+my_list = ["tokyo", "osaka", "fukuoka", "aichi", "kyoto", "chiba", "saitama", "gunma"]
+for i in my_list:
+    if len(i) >= 6:
+        print(i)
+    else:
+        pass
+
+# my_list = ["tokyo", "osaka", "fukuoka", "aichi", "kyoto", "chiba", "saitama", "gunma"]
+ 
+# [print(i) if len(i) >= 6 else None for i in my_list]
