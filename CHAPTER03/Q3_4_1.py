4ba46d508310af5cbf7a165bf423140e4de2a0f1
Yuto Kohama <s23009@std.it-college.ac.jp>
2023-07-13 07:42:59 +0000

    Q3_4_1 ~ Q3_6_1

---
+++ CHAPTER03/Q3_4_1.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+# 1, 3, 5, 7, 9
+
+for i in range(10):
+    if i % 2 == 0:
+        break
+    print(i)
+
+# None
---
+++ CHAPTER03/Q3_6_1.py
@@ -0,0 +1,12 @@
+import random
+
+a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+b = (random.sample(a,4))
+c = (''.join(b))
+while True:
+    val = input()
+    if val == c:
+        print('great!')
+        break
+    else:
+        print("Try!")
